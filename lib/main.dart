0b9c6babe8a195c346d365cb4e0cd15ae7da3c86
Edgar <edgar.jimenez23b@udgvirtual.udg.mx>
2026-04-19 17:12:32 -0600

    Agregué pantalla de inicio con imagen y bienvenida

--- lib/main.dart
+++ lib/main.dart
@@ -10,17 +10,71 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: const Center(
-          child: Text(
-            'Hello World',
-            style: TextStyle(fontSize: 24),
+      debugShowCheckedModeBanner: false,
+      title: 'Catálogo de Películas',
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+
+          // Imagen de fondo
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(
+                  'https://images.unsplash.com/photo-1489599849927-2ee91cede3ba'
+                ),
+                fit: BoxFit.cover,
+              ),
+            ),
           ),
-        ),
+
+          // Capa oscura para una mejor visibilidad en la app
+          Container(
+            color: Colors.black.withOpacity(0.5),
+          ),
+
+          // Contenido
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+
+                // Nombre de la app
+                Text(
+                  'Catálogo de Películas',
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+
+                SizedBox(height: 20),
+
+                // Mensaje de bienvenida
+                Text(
+                  'Bienvenido a la mejor app de películas',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white70,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
