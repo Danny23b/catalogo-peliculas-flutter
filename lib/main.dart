7b0576c290dad48d1ed2d5b5aaccc89de98f2706
Edgar <edgar.jimenez23b@udgvirtual.udg.mx>
2026-04-24 12:23:55 -0600

    Integración de API con HTTP (PokeAPI)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(const MyApp());
@@ -9,72 +11,65 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Catálogo de Películas',
-      home: const HomeScreen(),
+      home: PokemonScreen(),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class PokemonScreen extends StatefulWidget {
+  const PokemonScreen({super.key});
+
+  @override
+  State<PokemonScreen> createState() => _PokemonScreenState();
+}
+
+class _PokemonScreenState extends State<PokemonScreen> {
+  String name = "";
+  String image = "";
+
+  Future<void> fetchPokemon() async {
+    final url = Uri.parse('https://pokeapi.co/api/v2/pokemon/pikachu');
+
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+
+      setState(() {
+        name = data['name'];
+        image = data['sprites']['front_default'];
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchPokemon();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-
-          // Imagen de fondo
-          Container(
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: NetworkImage(
-                  'https://images.unsplash.com/photo-1489599849927-2ee91cede3ba'
-                ),
-                fit: BoxFit.cover,
+      appBar: AppBar(
+        title: const Text('API Pokémon'),
+      ),
+      body: Center(
+        child: name.isEmpty
+            ? const CircularProgressIndicator()
+            : Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    name.toUpperCase(),
+                    style: const TextStyle(fontSize: 24),
+                  ),
+                  const SizedBox(height: 20),
+                  Image.network(image),
+                ],
               ),
-            ),
-          ),
-
-          // Capa oscura para una mejor visibilidad en la app
-          Container(
-            color: Colors.black.withOpacity(0.5),
-          ),
-
-          // Contenido
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-
-                // Nombre de la app
-                Text(
-                  'Catálogo de Películas',
-                  style: TextStyle(
-                    fontSize: 30,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-
-                SizedBox(height: 20),
-
-                // Mensaje de bienvenida
-                Text(
-                  'Bienvenido a la mejor app de películas',
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: Colors.white70,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ],
-            ),
-          ),
-        ],
       ),
     );
   }
